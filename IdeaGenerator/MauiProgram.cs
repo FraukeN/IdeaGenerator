759fbe39c8b38af01a3d4f3ae30d3fc2df62dd46
Frauke <frauke@creativecats.com>
2023-07-25 12:49:55 +0100

    Tidy up in preparation for MVVM

--- IdeaGenerator/MauiProgram.cs
+++ IdeaGenerator/MauiProgram.cs
@@ -4,21 +4,15 @@ namespace IdeaGenerator;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
-
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
-
-		return builder.Build();
-	}
+    public static MauiApp CreateMauiApp()
+        => MauiApp
+        .CreateBuilder()
+        .UseMauiApp<App>()
+        .ConfigureFonts(
+            fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+        .Build();
 }
