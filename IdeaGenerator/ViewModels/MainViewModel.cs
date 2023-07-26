fc402b3a8f554bd89fff2ba7dfaa1edb92cf71cd
Frauke <frauke@creativecats.com>
2023-07-26 22:12:01 +0100

    Add seriously simplified MVVM

---
+++ IdeaGenerator/ViewModels/MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IdeaGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdeaGenerator.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _introduction;
+
+        [ObservableProperty]
+        private string _goal;
+
+        [ObservableProperty]
+        private string _villain;
+
+        [ObservableProperty]
+        private string _ally;
+
+        [ObservableProperty]
+        private string _patron;
+
+        [ObservableProperty]
+        private string _climax;
+        
+        AdventureModel adventureModel = new AdventureModel();
+
+        public MainViewModel()
+        {
+            _introduction = adventureModel.Introduction;
+            _goal = adventureModel.Goal;
+            _villain = adventureModel.Villain;
+            _ally = adventureModel.Ally;
+            _patron = adventureModel.Patron;
+            _climax = adventureModel.Climax;
+        }
+
+        [RelayCommand]
+        private void GenerateIdea()
+        {
+            Debug.WriteLine("Idea generated!");
+        }
+    }
+}
--- IdeaGenerator/Views/MainPage.xaml.cs
+++ IdeaGenerator/Views/MainPage.xaml.cs
@@ -1,24 +1,13 @@
-﻿namespace IdeaGenerator;
+﻿using IdeaGenerator.ViewModels;
+
+namespace IdeaGenerator;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		BindingContext = new MainViewModel();
 	}
 }
 
